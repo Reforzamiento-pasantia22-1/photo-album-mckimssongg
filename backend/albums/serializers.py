62e8dfbab2cdf5234da01a34db786f8ee78a81a2
mckimssongg <86619053+mckimssongg@users.noreply.github.com>
2022-05-26 13:33:37 -0600

    add

--- backend/albums/serializers.py
+++ backend/albums/serializers.py
@@ -3,9 +3,19 @@ from .models import *
 
 
 class AlbumSerializer(serializers.ModelSerializer):
+    images = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Album
-        fields = '__all__'
+        fields = ['id', 'user', 'name', 'description', 'is_public', 'images']
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['images'] = [{'id': image.id,
+                           'label': image.label,
+                           'link': image.link, }
+                          for image in instance.images.all()]
+        return data
 
 
 class ImageSerializer(serializers.ModelSerializer):
