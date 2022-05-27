c4d68a0a5077dca8d3c64385eee102227752504a
mckimssongg <86619053+mckimssongg@users.noreply.github.com>
2022-05-27 08:03:22 -0600

    add

--- backend/albums/urls.py
+++ backend/albums/urls.py
@@ -4,7 +4,7 @@ from django.db import router
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'albums', views.AlbumViewSet)
-router.register(r'image', views.ImageViewSet)
+router.register(r'albums', views.AlbumViewSet, basename='albums')
+router.register(r'image', views.ImageViewSet, basename='image')
 router.register(r'searchImage', views.SearchImageView, basename='searchImage')
 urlpatterns = router.urls
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
-router.register(r'users', views.UsersViewSet)
+router.register(r'users', views.UsersViewSet, basename='users')
 urlpatterns = router.urls
 
 
